d13233753624cd257eb5a7a2f92d93782b851005
labwons <>
2025-09-16 02:16:20 +0000

    COMMIT AND PUSH FROM COLAB

--- pylabwons/fetch/stock/tickers.py
+++ pylabwons/fetch/stock/tickers.py
@@ -13,63 +13,62 @@ import pandas as pd
 import requests
 
 
+RENAMER = {
+    "회사명":"name", 
+    "시장구분":"market", 
+    "종목코드":"ticker", 
+    "업종":"KRXIndustry",
+    "주요제품":"products", 
+    "상장일":"IPO",
+    '시가': 'open',
+    '고가': 'high',
+    '저가': 'low',
+    '종가': 'close',
+    '시가총액': 'marketCap',
+    '거래량': 'volume',
+    '거래대금': 'amount',
+    '상장주식수':'shares',
+    '보유수량': 'foreignersShares',
+    '지분율': 'foreignersRate',
+    '한도수량': 'exhaustionShares',
+    '한도소진률': 'exhsuationRate'
+}
+
+
 def get_corporations() -> DataFrame:
     try:
         html = requests.get('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download').text
         corp = pd.read_html(io=StringIO(html), encoding='euc-kr')[0]
         corp['종목코드'] = corp['종목코드'].astype(str).str.zfill(6)
         corp = corp.drop(columns=['대표자명', '홈페이지', '지역', '결산월'])
-        corp = corp.rename(columns={
-            "회사명":"name", 
-            "시장구분":"market", 
-            "종목코드":"ticker", 
-            "업종":"KRXIndustry",
-            "주요제품":"products", 
-            "상장일":"IPO"
-        })
+        corp = corp.rename(columns={key:val for key, val in RENAMER.items() if key in corp})
         corp['market'] = corp['market'].str.replace("코스닥", "KOSDAQ").replace("유가","KOSPI").replace("코넥스", "KONEX")
         corp = corp.set_index(keys='ticker')        
         return corp
     except (KeyError, Exception):
         return DataFrame()
 
-
-# def get_market_basics(date:str='') -> DataFrame:
-#     if not date:
-#         date = DateTime.trading
-#
-#     try:
-#         price =
-#
-#         caps = get_market_cap_by_ticker(date=date, market='ALL')
-#         caps = caps.rename(columns={
-#             '종가':'close',
-#             '시가총액':'marketCap',
-#             '거래량':'volume',
-#             '거래대금':'amount',
-#             '상장주식수':'shares'
-#         })
-#         return basics
-#     except (KeyError, Exception):
-#         return DataFrame()
-
-def get_market_caps(date:str='') -> DataFrame:
+def get_ohlcv(date:str='') -> DataFrame:
     if not date:
         date = DateTime.trading
     try:
-        caps = get_market_cap_by_ticker(date=date, market='ALL')
-        caps.index.name = 'ticker'
-        caps = caps.rename(columns={
-            '종가':'close',
-            '시가총액':'marketCap',
-            '거래량':'volume',
-            '거래대금':'amount',
-            '상장주식수':'shares'
-        })
-        return caps
+        data = get_market_ohlcv_by_ticker(date=date, market='ALL')
+        data.index.name = 'ticker'
+        data = data.rename(columns={key:val for key, val in RENAMER.items() if key in data})
+        return data
     except (KeyError, Exception):
         return DataFrame()
 
+def get_market_cap(date:str='') -> DataFrame:
+    if not date:
+        date = DateTime.trading
+    try:
+        data = get_market_cap_by_ticker(date=date, market='ALL')
+        data.index.name = 'ticker'
+        data = data.rename(columns={key:val for key, val in RENAMER.items() if key in data})
+        return data
+    except (KeyError, Exception):
+        return DataFrame()
 
 def get_foreigner_rate(date:str='') -> DataFrame:
     if not date:
@@ -77,13 +76,7 @@ def get_foreigner_rate(date:str='') -> DataFrame:
     try:
         data = get_exhaustion_rates_of_foreign_investment(date=date, market='ALL')
         data.index.name = 'ticker'
-        data = data.rename(columns={
-            '상장주식수':'shares',
-            '보유수량': 'foreignersShares',
-            '지분율': 'foreignersRate',
-            '한도수량': 'exhaustionShares',
-            '한도소진률': 'exhsuationRate'
-        })
+        data = data.rename(columns={key:val for key, val in RENAMER.items() if key in data})
         typecastInt32 = ['shares', 'foreignersShares', 'exhaustionShares']
         typecastFloat = ['foreignersRate', 'exhsuationRate']
         data[typecastInt32] = data[typecastInt32].astype('int32')
